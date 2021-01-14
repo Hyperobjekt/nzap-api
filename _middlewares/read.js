a5d1ec67517f74057c39a2d4ac3cba26713038f5
Uri Maxima <uri@maxima.ninja>
2021-01-14 09:21:35 -0700

    update routes

--- _middlewares/read.js
+++ _middlewares/read.js
@@ -2,7 +2,8 @@ module.exports = (req, res, next) => {
   const read = require('../_lib/ccrud/read');
   let db = req.app.get('db');
   let collection = req.baseUrl.split('/')[2];
-  read(db, req.schemas[collection], collection, req.locals.query, req.locals.skip, req.locals.limit, null, req.body.expand)
+
+  read(db, req.schemas[collection], collection, req.locals.query, req.locals.skip || 0, req.locals.limit || 200, null, req.body.expand)
     .then(response => {
       let paramKeys = Object.keys(req.params);
       paramKeys.length === 1 && paramKeys[0] === '_id'
