f58729194a3f0e00bc3eead47a505784ca4fc291
Uri Maxima <uri@maxima.ninja>
2021-02-23 10:49:38 -0700

    update filter parser

--- _scripts/mk_filters.js
+++ _scripts/mk_filters.js
@@ -22,20 +22,26 @@ const slugify = str => {
   return slug(str)
 }
 
-const extractLevelTwoFilters = (output, data) => {
+const assembleAndMapLevelTwoFilters = (output, data) => {
+  let tracker = []
+  let levelTwoFilters = []
   for (let i = 0; i < output.levelOneFilters.length; i++) {
-    let levelTwoFilters = []
-    data
-      .filter(e => e.filter_level_1 === output.levelOneFilters[i].label && !!e.filter_level_2)
-      .forEach(e => levelTwoFilters.indexOf(e.filter_level_2) === -1 ? levelTwoFilters.push(e.filter_level_2) : null)
-    output.levelOneFilters[i].levelTwoFilters = levelTwoFilters.map(e => { return { label: e, slug: slugify(e) } })
+    data.sort((a, b) => a._alt_l2 < b._alt_l2 ? -1 : 1).filter(e => e.filter_level_1 === output.levelOneFilters[i].label && !!e.filter_level_2).forEach(e => {
+      let trackerKey = `${e._filter_level_1}-${e._filter_level_2}`
+      if (tracker.indexOf(trackerKey) === -1) {
+        let filter = { l1: e.filter_level_1, l2: e.filter_level_2 }
+        tracker.push(trackerKey)
+        levelTwoFilters.push(filter)
+      }
+    })
+    output.levelTwoFilters = levelTwoFilters.map(e => { return { label: e.l2, slug: slugify(e.l2), levelOneLabel: e.l1, levelOneSlug: slugify(e.l1) } })
   }
   return saveOutput(output);
 }
 
 const processData = dataString => {
   let tempLevelOneFilters = [];
-  let data = JSON.parse(dataString);
+  let data = JSON.parse(dataString).sort((a, b) => a._alt_l1 < b._alt_l1 ? -1 : 1)
   for (let i = 0; i < data.length; i++) {
     let state = capitalize(data[i].geo)
     let year = data[i].year.replace(/\D/g, '')
@@ -48,7 +54,7 @@ const processData = dataString => {
   output.usStates = output.usStates.map(e => ({ label: e, slug: slugify(e) }))
   output.years = output.years.map(e => ({ label: e, slug: slugify(e) }))
   output.scenarios = output.scenarios.map(e => ({ label: e, slug: slugify(e) }))
-  return extractLevelTwoFilters(output, data)
+  return assembleAndMapLevelTwoFilters(output, data)
 }
 
 fs.readFile(path, async (err, data) => (err) ? console.error(err) : processData(data))
